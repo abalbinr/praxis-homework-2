9ad8d3a088fc4cb9d28b6b3b8862febf63054df3
Augusto Balbin <abalbinr@unal.edu.co>
2021-02-05 23:45:26 -0500

    Build the app and fix bugs

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,2 +1,36 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+
+#Git
+sudo yum install -y git
+git version
+
+#Go
+sudo yum install -y golang
+go version
+
+#Node and npm
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install -y nodejs
+node --version
+npm --version
+
+#Vue cli
+sudo npm install -g -y @vue/cli
+vue --version
+
+#Clonar repositorio de github de la app
+git clone https://github.com/jdmendozaa/vuego-demoapp.git
+
+#Build the backend
+cd vuego-demoapp/server
+go build -o /shared/greeter
+
+#Build the frontend
+#Ip del backend 10.0.0.8
+cd ../spa
+echo "VUE_APP_API_ENDPOINT=\"http://10.0.0.8:4001/api\"" > .env.production
+sudo npm install
+sudo npm run build
+tar czf dist.tgz dist/*
+mv dist.tgz /shared
+
